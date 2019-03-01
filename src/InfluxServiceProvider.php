109db9572d2c952ce6c241df87cf65ddb6deddc0
Salman Zafar <salman.zafar@innowi.com>
2019-03-01 15:32:26 +0500

    Influx laravel library create package structure

---
+++ src/InfluxServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: salman
+ * Date: 3/1/19
+ * Time: 3:12 PM
+ */
+
+namespace Salman\Influx;
+
+use Illuminate\Support\ServiceProvider;
+
+class InfluxServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->mergeConfigFrom(__DIR__.'config/influx.php','influx');
+        $this->publishes([
+
+            __DIR__.'config/influx.php' => config_path('influx.php')
+        ]);
+    }
+
+    public function register()
+    {
+
+    }
+}
---
+++ src/config/influx.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: salman
+ * Date: 3/1/19
+ * Time: 3:24 PM
+ */
+
+return [
+
+    "dbname"   => env('influx_dbname'),
+    "username" => env('influx_username',''),
+    "password" => env('influx_password',''),
+    "host"     => env('influx_host','localhost'),
+    "port"     => env('influx_port','8086')
+];
